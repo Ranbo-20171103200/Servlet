85259905daece37f9d65f95789079f00893fd5f7
Ranbo-20171103200 <43673449+Ranbo-20171103200@users.noreply.github.com>
2019-06-27 23:13:04 +0800

    Add files via upload

---
+++ RegisterServlet.java
@@ -0,0 +1,65 @@
+package cn.edu.imnu.RegisterServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.edu.imnu.User.User;
+import cn.edu.imnu.UserDAO.UserDAO;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+        /*设置响应头允许ajax跨域访问*/
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        /* 星号表示所有的异域请求都可以接受， */
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+        //获取微信小程序get的参数值并打印
+        String username = request.getParameter("name");
+        String password = request.getParameter("pwd");
+        boolean flag;
+        User user=new User();
+      	user.setUsername(username);
+        user.setPassword(password);
+        UserDAO dao=new UserDAO();
+        flag=dao.isExistUser(user);
+        if(flag==false)
+        {
+        	if(user.getUsername()!="")
+        	{
+        		dao.register(user);
+        	}
+        }
+	}
+
+}
